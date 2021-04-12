8a3c2bf7d31249caee73903975ee68507153ab6c
sonia-ko <sonya-ko@outlook.com>
2021-04-12 03:29:14 +0300

    Added a hamburger menu instead of the old menu

--- script.js
+++ script.js
@@ -3,17 +3,44 @@
 /* DECLARING VARIABLES FOR UI */
 
 const openWordChecker = document.getElementById('words-checker');
-const openItemsChecker = document.getElementById('items-checker');
 const welcomeArea = document.querySelector('.welcome-area');
 const wordsCheckerArea = document.querySelector('.words-checker');
 const calculatorArea = document.querySelector('.calculator-area');
 const logo = document.querySelector('.logo');
 const openCalculator = document.getElementById('calculator');
-
+const menuIcon = document.querySelector('.menu-container');
+const closeMenuBtn = document.querySelector('.close-menu-btn');
+let menuOpened = false;
 
 
 let operation;
 
+// a user opens the menu icon
+menuIcon.addEventListener('click', function(){
+    
+ if(menuOpened === false){
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  menuOpened = true;
+  menuIcon.classList.toggle("change");
+    }else{
+        closeNav();
+        
+    }
+    
+})
+
+closeMenuBtn.addEventListener('click', closeNav);
+
+
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  menuOpened = false;
+  menuIcon.classList.toggle("change");
+}
 
 // a user clicks on the logo:
 
